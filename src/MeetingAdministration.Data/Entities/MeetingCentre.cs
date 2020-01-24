9144273dcb41ccdac4418adf47c188e9a6237e7c
Daniel Jašek <jasekdan@gmail.com>
2020-01-24 22:57:25 +0100

    Raw design for main window. Moved app project folder into src folder.

--- src/MeetingAdministration.Data/Entities/MeetingCentre.cs
+++ src/MeetingAdministration.Data/Entities/MeetingCentre.cs
@@ -13,11 +13,17 @@ namespace MeetingAdministration.Data.Entities
             MeetingRooms = new List<MeetingRoom>();
         }
         public int Id { get; set; }
-
+        /// <summary>
+        /// Represents the name of the <see cref="MeetingCentre"/>. The value is restricted to &#8805; 2 and &#8804; 100 characters.
+        /// </summary>
         public string Name { get; set; }
-
+        /// <summary>
+        /// Represents a unique code used for this <see cref="MeetingCentre"/>. This is further restricted via regex.
+        /// </summary>
         public string Code { get; set; }
-
+        /// <summary>
+        /// Represents the description the <see cref="MeetingCentre"/>. The value is restricted to &#8805; 10; and &#8804; 300 characters.
+        /// </summary>
         public string Description { get; set; }
 
         public ICollection<MeetingRoom> MeetingRooms { get; set; }
--- src/MeetingAdministration.Data/Entities/MeetingRoom.cs
+++ src/MeetingAdministration.Data/Entities/MeetingRoom.cs
@@ -7,18 +7,27 @@ namespace MeetingAdministration.Data.Entities
     public class MeetingRoom
     {
         public int Id { get; set; }
-
+        /// <summary>
+        /// Represents the name of the <see cref="MeetingRoom"/>. The value is restricted to &#8805; 2 and &#8804; 100 characters.
+        /// </summary>
         public string Name { get; set; }
-
+        /// <summary>
+        /// Represents a unique code used for this <see cref="MeetingRoom"/>. This is further restricted via regex.
+        /// </summary>
         public string Code { get; set; }
-
+        /// <summary>
+        /// Represents the description the <see cref="MeetingRoom"/>. The value is restricted to &#8805; 10; and &#8804; 300 characters.
+        /// </summary>
         public string Description { get; set; }
 
         /// <summary>
-        /// This is restricted to &lt 100 people in it.
+        /// Represents the amount of people in the <see cref="MeetingRoom"/>, which is restricted to &#8805; 1 and &#8804; 100 people in it.
         /// </summary>
         public int Capacity { get; set; }
 
+        /// <summary>
+        /// Represents if this <see cref="MeetingRoom"/> is held as a VideoConference or not.
+        /// </summary>
         public bool IsVideoConference { get; set; }
 
         public MeetingCentre MeetingCentre { get; set; }
